8d9222f80766ab52a500e65081b17c7e80a3909a
McDonaldSebastine <116592576+McDonaldSebastine@users.noreply.github.com>
2023-03-14 14:59:06 +0100

    Update hardhat.config.js

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.7",
   networks:{
     goerli:{
-      url:'https://eth-goerli.g.alchemy.com/v2/6l8B3FdtNUIBfI92wStg45gpGx7Ef6a5', 
-      accounts: ["e38ebddf0dd85157ef6af30f6a1304f9ce962f1064c7cf5bbef2cb2cd26b0966"]
+      url:'NETWORK_API', 
+      accounts: ["PRIVATE_KEY"]
     }
   },
 
