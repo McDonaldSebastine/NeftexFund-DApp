bf035128ceb9296e18d670b13bf73c926d0a1a3b
McDonaldSebastine <116592576+McDonaldSebastine@users.noreply.github.com>
2023-03-15 20:22:28 +0100

    Update CampaignDetails.jsx

--- client/src/components/CampaignDetails.jsx
+++ client/src/components/CampaignDetails.jsx
@@ -6,24 +6,24 @@ import { withdrawcampaign } from "../utils/integration"
 
 const CampaignDetails = ({ campaign }) => {
     const [accountConnected] = useGlobalState("accountConnected")
-    const ended = new Date().getTime() > Number(campaign?.deadline + '000')
+    const ended = new Date().getTime() > Number (campaign?.deadline + '000')
     
   return (
     <div className="flex justify-center bg-gray-500 py-24 px-6">
         <div className="flex flex-col md:w-2/3">
             <div className="flex justify-start items-start sm:space-x-4 flex-wrap">
                 <img  
-                    src={project ?.imageURL}
-                    alt={project ?.title}
+                    src={campaign?.imageURL}
+                    alt={campaign?.title}
                     className="rounded-xl h-64 w-full sm:w-1/3 object-cover"
                 />
 
                 <div className="text-white flex-1 sm:py-0 py-4">
                     <div className="flex flex-col justify-start flex-wrap">
-                        <h5 className="text-xl font-semibold mb-2"> {campaign ?.title} </h5>
+                        <h5 className="text-xl font-semibold mb-2"> {campaign?.title} </h5>
                         <small className="text-white">
-                            {ended ? "Ended" 
-                            : daysRemaining(campaign ?.deadline) + "left"}
+                            {ended? "Ended" 
+                            : daysRemaining(campaign?.deadline) + "left"}
                         </small>
                     </div>
 
@@ -31,9 +31,9 @@ const CampaignDetails = ({ campaign }) => {
                         <div className="flex justify-between items-center w-full pb-2">
                             <div className="flex justify-start space-x-2 font-semibold mb-3">
                                 <Identicons className="rounded-full shadow-md"
-                                string={campaign ?.owner} size={20}/>
-                                {campaign ?. owner ? (
-                                    <small>{shorten (campaign ?.owner, 4, 4, 11) }</small>
+                                string={campaign?.owner} size={20}/>
+                                {campaign?. owner ? (
+                                    <small>{shorten (campaign?.owner, 4, 4, 11) }</small>
                                 ) : null}
 
                                 <small>{campaign?.supporters} Supporter {campaign?.supporters == 1 ? "" : "s"} </small>
@@ -48,7 +48,7 @@ const CampaignDetails = ({ campaign }) => {
                                 ) : campaign?.check == 3 ? (
                                     <small className="text-red-700">Terminated</small>
                                 ) : (  
-                                    <small className="text-teal-700">Withdrawn</small>
+                                    <small className="text-teal-400">Withdrawn</small>
                                 )} 
                        
                             </div>
@@ -57,12 +57,16 @@ const CampaignDetails = ({ campaign }) => {
                             <p className="text-white text-sm font-thin mt-1"> {campaign?.description}</p>
                             <div className="w-full bg-white mt-3">
                                 <div className="bg-purple-400 text-xs font-medium
-                                text-white text-center p-0.5 leading-none rounded-l-full"
-                                style={{width: `${(campaign?.amountAccrued / campaign?.amount) * 100 }%` }}>
+                                    text-white text-center p-0.5 leading-none rounded-l-full"
+                                    style={{width: `${(campaign?.amountAccrued / campaign?.amount) * 100 }%` }} 
+                                >
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-between items-center text-white font-semibold mt-1">
+
+                        <div className="flex justify-between items-center
+                         text-white font-semibold mt-1">
+                            
                             <small>{campaign?.Accrued} ETH Accrued</small>
                             <small className="flex justify-start items-center">
                                 <FaEthereum />
