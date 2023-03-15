7456beb4b314addf66d73efeab4b258007845081
McDonaldSebastine <116592576+McDonaldSebastine@users.noreply.github.com>
2023-03-15 20:11:52 +0100

    Update hardhat.config.js

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.7",
   networks:{
     goerli:{
-      url:'NETWORK_API', 
-      accounts: ["PRIVATE_KEY"]
+      url:'NETWORK_API', //input your Goerli API
+      accounts: ["PRIVATE_KEY"] //input your wallet private key ex. metamask wallet private key
     }
   },
 
